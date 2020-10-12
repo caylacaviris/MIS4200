cebf60ca895b4208d4082f8c456c723805bdc320
Matthew Hillard <mh019516@ohio.edu>
2020-10-12 14:28:21 -0400

    Created Model for User Data

---
+++ Team_1_Project/Models/userData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team_1_Project.Models
+{
+    public class userData
+    {
+        public Guid ID { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string fullName
+        {
+            get
+            {
+                return lastName + ", " + firstName;
+            }
+        }
+        public string officeLocation { get; set; }
+        public string position { get; set; }
+        public DateTime startDate { get; set; }
+    }
+}
