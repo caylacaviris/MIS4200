9e0ac3b3c3eb346b67cf21a9c4d6d684b120e034
Matthew Hillard <mh019516@ohio.edu>
2020-10-14 12:11:11 -0400

    Added validators and name changes to user fields

--- Team_1_Project/Models/userData.cs
+++ Team_1_Project/Models/userData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,19 @@ namespace Team_1_Project.Models
 {
     public class userData
     {
+
+        [Required]
         public Guid ID { get; set; }
+
+        [Required]
+        [Display (Name = "Last Name")]
         public string lastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         public string firstName { get; set; }
+
+        [Display(Name = "Full Name")]
         public string fullName
         {
             get
@@ -17,8 +28,24 @@ namespace Team_1_Project.Models
                 return lastName + ", " + firstName;
             }
         }
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string phoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display)Name = "Email"]
+        public string email { get; set; }
+
+        [Display(Name = "Office Location")]
         public string officeLocation { get; set; }
+
+        [Display(Name = "Position")]
         public string position { get; set; }
-        public DateTime startDate { get; set; }
+
+        [Display(Name = "Hire Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime hireDate { get; set; }
     }
 }
